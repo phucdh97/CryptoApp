9f91d309ceb6f7024e59b8b2d1295dbd22c68004
phucdh97 <phucdh97@gmail.com>
2025-04-27 13:35:23 +0700

    filtering coin

--- CryptoSwiftUI/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoSwiftUI/Core/Home/ViewModels/HomeViewModel.swift
@@ -21,10 +21,24 @@ class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] coins in
-                self?.allCoins = coins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] filterdCoins in
+                self?.allCoins = filterdCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        print("Run filter coins: \(text), coins: \(coins)")
+        guard !text.isEmpty else { return coins }
+        let lowercasedText = text.lowercased()
+        return coins.filter { coin in
+            coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
